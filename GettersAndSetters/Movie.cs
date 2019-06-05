0e3cff61a81c665af9a01e2be259d44ede428c2f
RAlanWright <wright.alan88@gmail.com>
2019-06-05 11:29:37 -0500

    Added Movie class, setter and getter.  Set up private rating to prevent output of invalid ratings for movies

---
+++ GettersAndSetters/Movie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettersAndSetters
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+        // Private can prevent the rating
+        // from being invalid
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            
+            get { return rating; }
+            set {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                {
+                    rating = value;
+                } else
+                {
+                    rating = "NR";
+                }
+
+                // Issue getting an array + for loop to work
+
+                //string[] ratingCheck = { "G", "PG", "PG-13", "R" };
+                //for (int i = 0; i < ratingCheck.Length; i++)
+                //{
+                //    if (value == ratingCheck[i])
+                //    {
+                //        rating = value;
+                //    }
+                //    else if (value != ratingCheck[i])
+                //    {
+                //        rating = "NR";
+                //    }
+                //}
+            }
+        }
+    }
+}
--- GettersAndSetters/Program.cs
+++ GettersAndSetters/Program.cs
@@ -11,6 +11,14 @@ namespace GettersAndSetters
         static void Main(string[] args)
         {
 
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            // G, PG, PG-13, R, NR
+            // if Movie.rating not in ratingCheck[], 
+            // then display error message
+            // utilize getts & setters
+
+            Console.WriteLine(avengers.Rating);
             Console.ReadLine();
         }
     }
